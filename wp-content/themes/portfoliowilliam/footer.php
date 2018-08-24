486153bdf9ffee7a7587e0b8dfac632a039505ae
JeMoeder <williamhartm@hotmail.com>
2018-08-24 21:31:15 +0200

    Dingen

--- wp-content/themes/portfoliowilliam/footer.php
+++ wp-content/themes/portfoliowilliam/footer.php
@@ -7,17 +7,17 @@
 	</div>
 </footer> -->
 
-<header class="lg-sticky row main-menu">
+<footer class="lg-sticky row main-menu">
     <div class="container">
-        <nav class="navbar navbar-expand-md">
-            <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <img class="logo-img" src="<?= get_template_directory_uri() . '/images/LOGO-POM-WIT.png'; ?>"
+        <nav class="navbar navbar-expand-md navbar-fixed-bottom">
+            <!-- <a class="navbar-brand" href="<? //php echo home_url(); ?>">
+                <img class="logo-img" src="<? //= get_template_directory_uri() . '/images/LOGO-POM-WIT.png'; ?>"
                      alt="logo"/>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
+            </a> -->
+            <button class="navbar-toggler dropup" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars hamburger_bars"></i>
+                <i class="fas fa-bars hamburger_bars dropup"></i>
             </button>
             <div class="collapse navbar-collapse mr-auto w-100 justify-content-end" id="navbarSupportedContent">
 			<? //php /* Primary navigation */
@@ -34,14 +34,14 @@
 					            'walker'          => new bs4navwalker(),
 				            ]);
 				?>
-                <div class="d-md-none my-lg-0 searchform mobilesf">
-					<?php get_search_form(); ?>
-                </div> 
+                <!-- <div class="d-md-none my-lg-0 searchform mobilesf">
+					<? //php get_search_form(); ?>
+                </div>  -->
             </div>
         </nav>
 
     </div>
-</header>
+</footer>
 
 <?php wp_footer(); ?>
 
