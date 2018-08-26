d20a69da228ee3e5965ddb890b4a5510c728ce0e
JeMoeder <williamhartm@hotmail.com>
2018-08-26 20:13:20 +0200

    AFBLIJVEN!!! BG VIDEO WERKT nog

--- wp-content/themes/portfoliowilliam/page.php
+++ wp-content/themes/portfoliowilliam/page.php
@@ -26,14 +26,13 @@
 </div> -->
 
 <div class="fullscreen-bg">
-    <video loop muted autoplay poster="img/videoframe.jpg" class="fullscreen-bg__video">
+    <video loop muted autoplay poster="video/eigenzinnig-web.jpg" class="fullscreen-bg__video">
         <!-- <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm" type="video/webm">
         <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4">
         <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv" type="video/ogg"> -->
-        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.webm" type="video/webm">
-        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.mp4" type="video/mp4">
-        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.ogv" type="video/ogg">
-        
+        <source src="<?=get_template_directory_uri() . '/video/eigenzinnig-web.webm'?>" type="video/webm">
+        <source src="<?=get_template_directory_uri() . '/video/eigenzinnig-web.mp4'?>" type="video/mp4">
+        <source src="<?=get_template_directory_uri() . '/video/eigenzinnig-web.ogv'?>" type="video/ogg">
     </video>
 </div>
 
