b61eb3ee98a612811a68b8f0ff7d4f1235de4ebd
JeMoeder <williamhartm@hotmail.com>
2018-08-26 18:31:34 +0200

    bg video

--- wp-content/themes/portfoliowilliam/page.php
+++ wp-content/themes/portfoliowilliam/page.php
@@ -18,10 +18,23 @@
     </div>
 </div> -->
 
-<div class="video-background">
+<!-- <div class="video-background">
     <div class="video-foreground">
         <iframe src="https://www.youtube.com/embed/WjuyR23sCrw?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=WjuyR23sCrw" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://giphy.com/embed/9GIEZnPsCkbbfzf4Ym" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/9GIEZnPsCkbbfzf4Ym">via GIPHY</a></p>
     </div>
+</div> -->
+
+<div class="fullscreen-bg">
+    <video loop muted autoplay poster="img/videoframe.jpg" class="fullscreen-bg__video">
+        <!-- <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm" type="video/webm">
+        <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" type="video/mp4">
+        <source src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv" type="video/ogg"> -->
+        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.webm" type="video/webm">
+        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.mp4" type="video/mp4">
+        <source src="/Applications/XAMPP/xamppfiles/htdocs/William/wp-content/themes/portfoliowilliam/eigenzinnig-web.ogv" type="video/ogg">
+        
+    </video>
 </div>
 
 <?php get_footer(); ?>
