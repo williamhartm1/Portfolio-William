7bf1306e907c84b3d3fa00f377b621b110df2cbb
JeMoeder <williamhartm@hotmail.com>
2018-08-27 18:56:46 +0200

    skills&experience page

--- wp-content/themes/portfoliowilliam/Page Template/skills-experience.php
+++ wp-content/themes/portfoliowilliam/Page Template/skills-experience.php
@@ -9,8 +9,8 @@
     <div class="row">
         <div class="container container-work">
             <div class="posts">
-                <div class="row projecten-row">
-                    <div class="col-sm-12 col-md-6">
+                <div class="row">
+                    <div class="col-sm-12 col-md-8">
 						<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
                             <div class="post project-post">
                                 <h1><?php the_title(); ?></h1>
@@ -21,56 +21,40 @@
 							echo "No posts found.";
 						endif;
 						?>
-                    </div>
-                    <div class="col no-padding">
-						<?php
-						$post_counter = 1;
-						$first_post   = true;
-						global $post;
-						$args      = array(
-							'category_name'  => 'work',
-							'order'          => 'name',
-							'orderby'        => 'date',
-							'posts_per_page' => -1,
-						);
-						$postslist = get_posts($args);
-						foreach($postslist
-
-						as $post):
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<?php
+					global $post;
+					$args      = array(
+						'category_name'  => 'skills&experience',
+						'order'          => 'DESC',
+						'orderby'        => 'date',
+						'posts_per_page' => '-1',
+					);
+					$postslist = get_posts($args);
+					foreach($postslist as $post):
 						setup_postdata($post);
-						if($first_post === true): ?>
-                        <div class="align-self-center">
-							<?php if(has_post_thumbnail()): ?>
-                                <div class="project-img">
-                                    <a href="<?= get_post_permalink(); ?>">
-                                        <img src="<?= the_post_thumbnail_url(); ?>" alt="work image">
-                                    </a>
-                                </div>
-							<?php endif; ?>
-                        </div>
-						<?php $first_post = false; ?>
-                    </div>
-					<?php else: ?>
-                        <div class="col-12 col-sm-3 align-self-center no-padding">
-							<?php if(has_post_thumbnail()): ?>
-                                <div class="project-img">
-                                    <a href="<?= get_post_permalink(); ?>">
-                                        <img src="<?= the_post_thumbnail_url(); ?>" alt="work image">
-                                    </a>
-                                </div>
-							<?php endif; ?>
-                        </div>
-						<?php
-						$post_counter++;
-					endif;
-					if($post_counter % 4 == 0 && ($post_counter < (count($postslist) - 1))) {
-						echo "</div>";
-						echo "<div class='row projecten-row'>";
-					}
+						?>
+						<div class="col-12 col-sm-6">
+							<div class="post">
+								<div class="row">
+									<div class="col">
+										<h2 class="sub-title"><?php the_title(); ?></h1>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col">
+										<div><?php the_content(); ?></div>
+									</div>
+								</div>
+							</div>
+					<?php
 					endforeach;
 					wp_reset_postdata();
 					?>
-                </div>
+				</div>
             </div>
         </div>
     </div>
