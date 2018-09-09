bef42cd4c1183ec4de366d76ed3182ea6e651159
Ruben <thewheelz14@gmail.com>
2018-09-09 18:49:37 +0200

    Zieke update voor work posts loop

--- wp-content/themes/portfoliowilliam/Page Template/work_new.php
+++ wp-content/themes/portfoliowilliam/Page Template/work_new.php
@@ -11,70 +11,66 @@
             <div class="posts">
                 <div class="row projecten-row align-items-end">
                     <div class="col-sm-6 col-md-6 no-padding">
-                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
                             <div class="post project-post">
                                 <h1><?php the_title(); ?></h1>
-                                <?php the_content(); ?>
+								<?php the_content(); ?>
                             </div>
-                        <?php endwhile;
-                        else:
-                            echo "No posts found.";
-                        endif;
-                        ?>
+						<?php endwhile;
+						else:
+							echo "No posts found.";
+						endif;
+						?>
                     </div>
+					
+					<?php
+					global $post;
+					$args = array(
+						'category_name'  => 'work',
+						'order'          => 'ASC',
+						'posts_per_page' => -1,
+					);
+					$temp_postslist = get_posts($args);
+					$postslist = [];
+					
+					for($i = 0; $i <= count($temp_postslist); $i++) {
+						$post = $temp_postslist[$i];
+						setup_postdata($post);
+						if(!has_post_thumbnail() || !get_the_post_thumbnail()) {
+							unset($temp_postslist[$i]);
+						}
+						wp_reset_postdata();
+					}
+					shuffle($temp_postslist);
+					$postslist = $temp_postslist;
+					
+					//TODO: sometimes there is only one post in the first row
+					
+					for($i = 0; $i <= count($postslist); $i++):
+						$post = $postslist[$i];
+						setup_postdata($post);
 
-                    <?php
-                    $post_counter = 1;
-                    $first_post = true;
-                    global $post;
-                    $args = array(
-                        'category_name'  => 'work',
-                        //'order'          => 'name',
-                        'orderby'        => 'rand',
-                        'posts_per_page' => -1,
-                    );
-                    $postslist = get_posts($args);
-
-                    for ($i = 0; $i <= 1; $i++):
-                        $post = $postslist[$i];
-                        setup_postdata($post);
-                        ?>
-                        <div class="col-12 col-sm-3 no-padding">
-                            <div class="align-self-center">
-                                <?php if (has_post_thumbnail()): ?>
+						if(has_post_thumbnail() && get_the_post_thumbnail()): ?>
+                            <div class="col-12 col-sm-3 no-padding">
+                                <div class="align-self-center">
                                     <div class="project-img">
                                         <a href="<?= get_post_permalink(); ?>">
                                             <img src="<?= the_post_thumbnail_url(); ?>" alt="work image">
                                         </a>
                                     </div>
-                                <?php endif; ?>
+                                </div>
                             </div>
-                        </div>
-                    <?php
-                    endfor;
-
-                    echo '</div>';
-                    echo '<div class="row projecten-row">';
-
-                    for ($i = 2; $i <= count($postslist); $i++):
-                        $post = $postslist[$i];
-                        setup_postdata($post);
-                        ?>
-                        <div class="col-12 col-sm-3 no-padding">
-                            <div class="align-self-center">
-                                <?php if (has_post_thumbnail()): ?>
-                                    <div class="project-img">
-                                        <a href="<?= get_post_permalink(); ?>">
-                                            <img src="<?= the_post_thumbnail_url(); ?>" alt="work image">
-                                        </a>
-                                    </div>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    <?php
-                    endfor;
-                    wp_reset_postdata();
-                    ?>
+							<?php
+							if($i == 1):
+								//puts a new row in the page after the first 2 posts (post 0 and 1)
+								echo '</div>';
+								echo '<div class="row projecten-row">';
+							endif;
+						endif;
+						?>
+					<?php
+					endfor;
+					?>
                 </div>
             </div>
         </div>
