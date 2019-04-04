ce87c93abbe2d627566dc89b41b6617d841961ad
JeMoeder <williamhartm@hotmail.com>
2019-04-04 18:17:08 +0200

    shit werkt met wp warning
    test die shit

--- wp-content/themes/portfoliowilliam/front-page.php
+++ wp-content/themes/portfoliowilliam/front-page.php
@@ -1,18 +1,9 @@
 <?php get_header(); ?>
 
-<div class="container-fp">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="text-blocks">
-                    <div class="text-block-left">
-                        <?php dynamic_sidebar('txt-block-left') ?>
-                    </div>
-                    <div class="text-block-right">
-                        <?php dynamic_sidebar('txt-block-right') ?>
-                    </div>
-                </div>
-            </div>
+<div class="container">
+    <div class="row h-100">
+        <div class="col-sm-12 my-auto">
+            <div class="card card-block">I am Groot.</div>
         </div>
     </div>
 </div>
