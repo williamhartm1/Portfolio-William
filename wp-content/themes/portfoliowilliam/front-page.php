4fc2963e61cb9e1ce72e69c8b7417324baa1e021
JeMoeder <williamhartm@hotmail.com>
2019-04-05 21:00:57 +0200

    HET WERKTT TSSSSUUUUUUUU

--- wp-content/themes/portfoliowilliam/front-page.php
+++ wp-content/themes/portfoliowilliam/front-page.php
@@ -1,9 +1,20 @@
 <?php get_header(); ?>
 
-<div class="container">
-    <div class="row h-100">
-        <div class="col-sm-12 my-auto">
-            <div class="card card-block">I am Groot.</div>
+<div class="container-fp">
+    <div class="container">
+        <div class="row h-100">
+            <div class="col-12 h-100 d-table">
+                <div class="d-table-cell align-middle">
+                    <div class="text-blocks">
+                        <div class="text-block-left">
+                            <?php dynamic_sidebar('txt-block-left') ?>
+                        </div>
+                        <div class="text-block-right">
+                            <?php dynamic_sidebar('txt-block-right') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
