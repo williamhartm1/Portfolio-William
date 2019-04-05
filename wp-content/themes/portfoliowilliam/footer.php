ec51143085e5a9b0ef4b66ae25f0ea7123201c5a
JeMoeder <williamhartm@hotmail.com>
2019-04-05 21:00:26 +0200

    shit opgeruimd

--- wp-content/themes/portfoliowilliam/footer.php
+++ wp-content/themes/portfoliowilliam/footer.php
@@ -1,25 +1,9 @@
 </div>
-<!-- <footer>
-	<div class="container">
-		<div class="copyrights text-center">
-			<? //php dynamic_sidebar( 'fw-1' ) ?>
-		</div>
-	</div>
-</footer> -->
 <?php include 'contact.php';?>
 
 <footer class="lg-sticky row main-menu">
     <div class="container container-footer">
         <nav class="navbar navbar-expand-md fixed-bottom">
-            <!-- <a class="navbar-brand" href="<? //php echo home_url(); ?>">
-                <img class="logo-img" src="<? //= get_template_directory_uri() . '/images/LOGO-POM-WIT.png'; ?>"
-                     alt="logo"/>
-            </a> -->
-            <!-- <button type="button" class="navbar-toggler dropup  float-xs-right" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars hamburger_bars dropup"></i>
-			</button> -->
 			<button type="button" class="navbar-toggler ml-auto hidden-sm-up float-xs-right m-3" data-toggle="collapse" 
 					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 					aria-expanded="false" aria-label="Toggle navigation">
@@ -40,17 +24,6 @@
 					            'walker'          => new bs4navwalker(),
 				            ]);
 				?>
-                <!-- <div class="d-md-none my-lg-0 searchform mobilesf">
-					<? //php get_search_form(); ?>
-                </div>  -->
-				<!-- <li id="menu-item-95" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-95 nav-item"> -->
-					<!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-						Launch demo modal
-					</button> -->
-					<!-- <a class="li-modal" data-toggle="modal" data-target="#modal">
-						CONTACT
-					</a>
-				</li> -->
             </div>
         </nav>
     </div>
